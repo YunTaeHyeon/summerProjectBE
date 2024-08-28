49a8f459b43eb75892d6e5af5612aa2965c7ba08
t05h19@naver.com <t05h19@naver.com>
2024-08-28 18:37:33 +0900

    feat: JWT 인증 가능하게 설정

--- src/main/java/com/summer/be/SwaggerConfig.java
+++ src/main/java/com/summer/be/SwaggerConfig.java
@@ -1,27 +1,32 @@
 package com.summer.be;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import lombok.RequiredArgsConstructor;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "오늘의 스피킹 API 명세서",
-                description = "서머 프로젝트 오늘의 스피킹 API 명세서",
-                version = "v1"))
-@RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public GroupedOpenApi chatOpenApi() {
-        // "/v1/**" 경로에 매칭되는 API를 그룹화하여 문서화한다.
-        String[] paths = {"/v1/**"};
+    private static final String SECURITY_SCHEME_NAME = "authorization";	// 추가
 
-        return GroupedOpenApi.builder()
-                .group("서머 프로젝트 v1")  // 그룹 이름을 설정한다.
-                .pathsToMatch(paths)     // 그룹에 속하는 경로 패턴을 지정한다.
-                .build();
+    @Bean
+    public OpenAPI swaggerApi() {
+        return new OpenAPI()
+                .components(new Components()
+                        // 여기부터 추가 부분
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                // 여기까지
+                .info(new Info()
+                        .title("오늘의 스피킹 API 명세서")
+                        .description("서머 프로젝트 오늘의 스피킹 API 명세서!!!")
+                        .version("1.0.0"));
     }
 }
