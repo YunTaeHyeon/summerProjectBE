b6cc32e8b755757672e561afd136cab1cb54ac94
bukwon <dnu05043@gmail.com>
2024-09-01 17:32:55 +0900

    fix: fix SetencesRepository

--- src/main/java/com/summer/be/openai/dao/SentencesRepository.java
+++ src/main/java/com/summer/be/openai/dao/SentencesRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SentencesRepository extends JpaRepository<Sentences, Long> {
     // 최근에 저장된 vocabulary의 영어 단어 리스트를 가져오는 쿼리
-    @Query("SELECT s.english_sentence FROM Sentences s WHERE s.id = :id")
+    @Query("SELECT s.sentences FROM Sentences s WHERE s.id = :id")
     String findSentencesById(Long id);
 }
